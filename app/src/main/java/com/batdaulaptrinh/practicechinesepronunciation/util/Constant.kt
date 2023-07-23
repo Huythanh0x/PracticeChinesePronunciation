430d2a8f47aa8936ee8af1d1f03523f806e3fbe3
vfa-thanhvh <vfa.internship.thanhvh@gmail.com>
2023-07-23 10:54:33 +0700

    #1001 add util classes

---
+++ app/src/main/java/com/batdaulaptrinh/practicechinesepronunciation/util/Constant.kt
@@ -0,0 +1,12 @@
+package com.batdaulaptrinh.practicechinesepronunciation.util
+
+class Constant {
+    companion object {
+        const val NEW_SPEECH_TABLE_NAME = "new_speech_table"
+        const val BASE_URL_API = "https://raw.githubusercontent.com/Huythanh0x/hanzi_database/master"
+        const val DATA_STORE_NAME = "new_speech-datastore"
+        const val NETWORK_AUTHORIZATION_HEADER = "Authorization"
+        const val NETWORK_BEARER_PREFIX = "Bearer"
+        const val ITEMS_PER_PAGE = 10
+    }
+}
---
+++ app/src/main/java/com/batdaulaptrinh/practicechinesepronunciation/util/network/NetworkStatusCode.kt
@@ -0,0 +1,8 @@
+package com.batdaulaptrinh.practicechinesepronunciation.util.network
+
+object NetworkStatusCode {
+    const val HTTP_CODE_UNAUTHORIZED = 401
+    const val HTTP_CODE_BAD_REQUEST = 400
+    const val HTTP_CODE_OK = 200
+    const val HTTP_CODE_INTERNAL_ERROR = 403
+}
