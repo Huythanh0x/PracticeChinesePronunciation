3c951e1c007d724f50bac4046d6e29eefcf8ff60
vfa-thanhvh <vfa.internship.thanhvh@gmail.com>
2023-07-29 21:52:11 +0700

    #1010 - bind data to speech recyclerview

--- app/src/main/java/com/batdaulaptrinh/practicechinesepronunciation/presentation/ui/list/speech/SpeechFragment.kt
+++ app/src/main/java/com/batdaulaptrinh/practicechinesepronunciation/presentation/ui/list/speech/SpeechFragment.kt
@@ -1,4 +1,4 @@
-package com.batdaulaptrinh.practicechinesepronunciation.presentation.ui.speech
+package com.batdaulaptrinh.practicechinesepronunciation.presentation.ui.list.speech
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,19 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.batdaulaptrinh.practicechinesepronunciation.databinding.FragmentSpeechBinding
+import com.batdaulaptrinh.practicechinesepronunciation.presentation.adapter.SpeechRecyclerViewAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SpeechFragment : Fragment() {
     private var _binding: FragmentSpeechBinding? = null
     private val binding get() = _binding!!
     private val navArgs: SpeechFragmentArgs by navArgs()
+    private val speechViewModel: SpeechViewModel by viewModels()
+    lateinit var recyclerViewAdapter: SpeechRecyclerViewAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSpeechBinding.inflate(inflater, container, false)
-        Toast.makeText(requireContext(), navArgs.lessonTitle, Toast.LENGTH_LONG).show()
+        recyclerViewAdapter = SpeechRecyclerViewAdapter(mutableListOf()) {
+            Toast.makeText(requireContext(), it.chinese, Toast.LENGTH_LONG).show()
+        }
+        binding.rvSpeeches.adapter = recyclerViewAdapter
+        speechViewModel.speeches.observe(viewLifecycleOwner) {
+            recyclerViewAdapter.setList(it)
+        }
+        speechViewModel.loadSpeeches(navArgs.lessonTitle)
         val lessonTitle = navArgs.lessonTitle.split(" ").subList(0,2).joinToString(": ")
         binding.tvLessonTitle.text = lessonTitle
         return binding.root
