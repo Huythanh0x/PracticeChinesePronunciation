fc1e90b6976f5657170c5910f0b229069c59c278
vfa-thanhvh <vfa.internship.thanhvh@gmail.com>
2023-07-31 13:13:51 +0700

    #1011 - add download language if missing

--- app/src/main/java/com/batdaulaptrinh/practicechinesepronunciation/domain/logic/TTS.kt
+++ app/src/main/java/com/batdaulaptrinh/practicechinesepronunciation/domain/logic/TTS.kt
@@ -1,9 +1,11 @@
 package com.batdaulaptrinh.practicechinesepronunciation.domain.logic
 
 import android.app.Activity
+import android.content.Intent
 import android.speech.tts.TextToSpeech
 import android.widget.Toast
 import java.util.Locale
+
 
 class TTS(
     private val activity: Activity,
@@ -17,8 +19,9 @@ class TTS(
             val localeZh = Locale("zh", "CN", "Hans")
             val result = tts.setLanguage(localeZh)
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Toast.makeText(activity, "This Language is not supported", Toast.LENGTH_SHORT)
-                    .show()
+                val installIntent = Intent()
+                installIntent.action = TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA
+                activity.startActivity(installIntent)
             } else {
                 speakOut(message)
             }
